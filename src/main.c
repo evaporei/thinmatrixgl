ff5ab817911005d2260009ea5a7c7577e261ba22
Eva Pace <eba.pachi@gmail.com>
2024-11-24 12:19:37 -0300

    open window & init opengl

--- src/main.c
+++ src/main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+void process_input(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, 1);
+    }
+}
+
 int main(void) {
-    printf("game\n");
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "3D game", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (gladLoadGL() == GL_FALSE)
+        return -1;
+
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
